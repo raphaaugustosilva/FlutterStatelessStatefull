07f82b2a868b6e313536ff57158b61e91474d78e
Raphael Augusto da Silva <raphaaugustosilva@gmail.com>
2019-05-23 16:08:03 -0300

    Testando codemagic

--- teste_stateless_statefull/lib/view/statefulView.dart
+++ teste_stateless_statefull/lib/view/statefulView.dart
@@ -43,6 +43,7 @@ class _StatefulViewState extends State<StatefulView> {
                 setState(() {
                   _numeroRandomicoResultado = obtemNumeroRandomico();
                 });
+                
               },
             ),
           ],
